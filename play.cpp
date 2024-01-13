09abc4d391d746e69b60f8213b6e9ba8459fe982
msmouni <marwan-smouni@hotmail.fr>
2024-01-13 20:26:36 +0100

    Disable `mouseEvent`s on `PlayView`

--- play.cpp
+++ play.cpp
@@ -5,9 +5,9 @@ PlayView::PlayView(QWidget *parent) : QGraphicsView(parent)
     this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
+    // Scene
     m_scene = new QGraphicsScene(this);
     this->setScene(m_scene);
-
     m_scene->setBackgroundBrush(Qt::blue); // TMP
 
     // Tiles
@@ -44,16 +44,25 @@ PlayView::PlayView(QWidget *parent) : QGraphicsView(parent)
     m_scene->addItem(enemy);
     m_enemies.append(enemy);
 
+    // Update
     m_update_timer = new QTimer;
-
     m_update_timeout_ms = 50;
-
     m_update_timer->setInterval(m_update_timeout_ms);
     m_update_timer->start();
     connect(m_update_timer, SIGNAL(timeout()), this, SLOT(updateItems()));
 }
 
 void PlayView::wheelEvent(QWheelEvent *event)
+{
+    event->ignore();
+}
+
+void PlayView::mousePressEvent(QMouseEvent *event)
+{
+    event->ignore();
+}
+
+void PlayView::mouseDoubleClickEvent(QMouseEvent *event)
 {
     event->ignore();
 }
--- play.h
+++ play.h
@@ -16,6 +16,8 @@ public:
 
 protected:
     void wheelEvent(QWheelEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
 
 private:
     QGraphicsScene *m_scene;
