3d24f21342fbbcb4a36a281e7284f75f96699f7e
msmouni <marwan-smouni@hotmail.fr>
2024-01-17 22:59:31 +0100

    Add `PathFinding` to Enemy class

--- enemy.cpp
+++ enemy.cpp
@@ -3,9 +3,28 @@
 Enemy::Enemy(const QRectF &rect, const QString &res_path, const Platform &platform, const QRectF &player_rect) : Character(rect, res_path, platform), m_player_rect(player_rect)
 {
     m_type = CharacterType::Enemy;
+
+    connect(&m_path_finder, SIGNAL(pathFindingRes(QVector<QPoint>)), this, SLOT(setPathFindingResult(QVector<QPoint>)));
 }
 
 void Enemy::gameUpdate()
 {
+    if (isOnGround())
+    {
+        m_path_finder.findPath(scenePos(), m_player_rect.center());
+    }
+
+    followPath();
     updateCharacter();
+}
+
+void Enemy::setPathFindingResult(QVector<QPoint> path)
+{
+    QMutexLocker ml(&m_path_mutex);
+    m_path_tiles = path;
+}
+
+void Enemy::followPath()
+{
+    QMutexLocker ml(&m_path_mutex);
 }
--- enemy.h
+++ enemy.h
@@ -2,16 +2,28 @@
 #define ENEMY_H
 
 #include "character.h"
+#include "path_finding.h"
+#include <QMutex>
 
 class Enemy : public Character
 {
+    Q_OBJECT
 public:
     Enemy(const QRectF &rect, const QString &res_path, const Platform &platform, const QRectF &player_rect);
 
     void gameUpdate() override;
 
+protected slots:
+    void setPathFindingResult(QVector<QPoint>);
+
 private:
     const QRectF &m_player_rect;
+    PathFinder m_path_finder;
+    QVector<QPoint> m_path_tiles;
+    // The mutex is locked when QMutexLocker is created. If locked, the mutex will be unlocked when the QMutexLocker is destroyed.
+    QMutex m_path_mutex;
+
+    void followPath();
 };
 
 #endif // ENEMY_H
